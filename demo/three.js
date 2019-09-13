6d4d2303948631fb453b987a10bd9e4d50bcec50
Aki Rodic <aleksandar.xyz@gmail.com>
2019-09-13 17:22:58 +0100

    Updated three.js demo.

--- demo/three.js
+++ demo/three.js
@@ -9,9 +9,12 @@ import {
 	BoxBufferGeometry,
 	MeshLambertMaterial,
 	Mesh,
+	Vector3,
+	HemisphereLight
 } from "../../../three.js/build/three.module.js";
 import {OrbitControls} from "../../../three.js/examples/jsm/controls/OrbitControls.js";
 import {TransformControls} from "../../../three.js/examples/jsm/controls/TransformControls.js";
+import {GLTFLoader} from "../../../three.js/examples/jsm/loaders/GLTFLoader.js";
 
 export class IoDemoThree extends IoElement {
 	static get Style() {
@@ -54,8 +57,8 @@ export class IoDemoThree extends IoElement {
 	}
 	onResized() {
 		this.camera.aspect = this.size[0] / this.size[1];
-		this.camera.near = 0.1;
-		this.camera.far = 100000;
+		this.camera.near = 0.01;
+		this.camera.far = 10000;
 		this.camera.updateProjectionMatrix();
 		this.renderer.setSize(this.size[0], this.size[1]);
 		this.render();
@@ -67,7 +70,23 @@ export class IoDemoThree extends IoElement {
 			['io-three-inspector', {id: 'inspector', value: this}],
 		]);
 
+		this.renderer.gammaFactor = 2.2;
+		this.renderer.gammaInput = true;
+		this.renderer.gammaOutput = true;
+
 		this.render = this.render.bind(this);
+
+		const loader = new GLTFLoader();
+
+		const scene = this.scene;
+		loader.load('/three-ui/demo/scene/cubes.gltf', gltf => {
+			gltf.scene.children.forEach(child => { scene.add( child ); });
+			scene.add(new HemisphereLight(0x333333, 0xffffff, 3));
+			window.dispatchEvent(new CustomEvent('object-mutated', {detail: {object: scene.children}}));
+		}, undefined, function ( e ) {
+			console.error( e );
+		} );
+		loader.manager.onLoad = this.render;
 
 		this.init();
 		this.render();
@@ -80,54 +99,40 @@ export class IoDemoThree extends IoElement {
 		renderer.domElement.setAttribute('tabindex', 0);
 
 		const scene = this.scene;
-		scene.add( new GridHelper( 1000, 10 ) );
+		scene.add( new GridHelper( 10, 10 ) );
 
 		const camera = this.camera;
-		camera.position.set( 1000, 500, 1000 );
-		camera.lookAt( 0, 200, 0 );
+		camera.position.set(2, 2, 2);
+		camera._target = new Vector3(0, 1, 0);
+		camera.lookAt( camera._target );
 		scene.add( camera );
-
-		const light = new DirectionalLight( 0xffffff, 2 );
-		light.position.set( 1, 1, 1 );
-		scene.add( light );
-
-		const texture = new TextureLoader().load( 'images/logo/io-512.png', this.render );
-		texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
-
-		const geometry = new BoxBufferGeometry( 200, 200, 200 );
-		const material = new MeshLambertMaterial( { map: texture, transparent: true } );
-
-		const mesh = new Mesh( geometry, material );
-		scene.add( mesh );
 
 		const orbit = new OrbitControls( camera, renderer.domElement );
 		orbit.update();
 		orbit.addEventListener( 'change', () => {
-			const selected = this.$.inspector.selected;
-			this.dispatchEvent('object-mutated', {object: selected}, false, window);
+			this.dispatchEvent('object-mutated', {object: camera}, false, window);
+			this.dispatchEvent('object-mutated', {object: camera.position}, false, window);
+			this.dispatchEvent('object-mutated', {object: camera.rotation}, false, window);
+			this.dispatchEvent('object-mutated', {object: camera.quaternion}, false, window);
 			this.render();
 		} );
 
-		const control = new TransformControls( camera, renderer.domElement );
-		control.addEventListener( 'change', () => {
-			const selected = this.$.inspector.selected;
-			this.dispatchEvent('object-mutated', {object: selected}, false, window);
-			this.render();
-		} );
+		// const control = new TransformControls( camera, renderer.domElement );
+		// control.addEventListener( 'change', () => {
+		// 	const selected = this.$.inspector.selected;
+		// 	this.dispatchEvent('object-mutated', {object: selected}, false, window);
+		// 	this.render();
+		// } );
 
-		control.addEventListener( 'dragging-changed', ( event ) => {
-			orbit.enabled = ! event.value;
-		} );
-
-		control.attach( mesh );
-		scene.add( control );
+		// control.addEventListener( 'dragging-changed', ( event ) => {
+		// 	orbit.enabled = ! event.value;
+		// } );
 
 		this.$.inspector.addEventListener( 'change', () => {
 			this.render();
 		} );
 
 		this.$.inspector.value = scene.children;
-		// window.object = this.$.inspector.value; // DEBUG
 	}
 }
 
